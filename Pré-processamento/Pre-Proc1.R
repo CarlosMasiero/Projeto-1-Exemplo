b6e2656f9d58f4ec182f55e7c0a66aa91dd2cf58
Carlos Masiero <carlos_masiero@hotmail.com>
2021-06-28 19:25:10 -0300

    Arquivos de pre-processamento

---
+++ Pré-processamento/Pre-Proc1.R
@@ -0,0 +1,10 @@
+# Não use diretórios com espaço no nome
+setwd("C:/FCD/BigDataRAzure/Cap05")
+getwd()
+
+# Usando as funções base do R (pacote utils)
+search()
+
+
+# Importando arquivo com read.table()
+?read.table
